8f0b14266a10f71d8d83beab1ce23b9d2800d74d
VladTwoDollars22 <burkenavladislav@gmail.com>
2023-08-14 16:11:47 +0300

    Update Aboba.cs

--- DZ/Assets/Aboba.cs
+++ DZ/Assets/Aboba.cs
@@ -7,7 +7,7 @@ public class Aboba : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Sraka
     }
 
     // Update is called once per frame
